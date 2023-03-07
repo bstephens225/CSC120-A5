fe25759dbddc12b590a36cc83302025f37431c6f
bstephens225 <bstephens@smith.edu>
2023-03-06 21:03:21 -0500

    constructor and some methods

--- Train.java
+++ Train.java
@@ -1,3 +1,46 @@
-public class Train {
+import java.util.ArrayList;
 
+public class Train {
+    private final Engine engine;
+    private ArrayList<Car> cars;
+
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity) {
+        Engine e= new Engine(fuelType, fuelCapacity);
+        engine = e;
+        for (int i=0;i<nCars;i++){
+            Car c = new Car(passengerCapacity);
+            cars.add(c);
+        }
+    }
+    public Engine getEngine(){
+        return engine;
+    }
+    public Car getCar(int i){
+        return cars.get(i);
+    } //to return the ith car
+
+    public int getMaxCapacity() {
+        return 1;
+    }//which will return the maximum total capacity across all Cars
+
+    public int seatsRemaining() {
+        return 1;
+    }//which will return the number of remaining open seats across all Cars
+    
+    public void printManifest(){
+        cars.forEach(car->car.printManifest());
+    } //that prints a roster of all Passengers on board
 }
+
+
+/**To assemble your Train, you'll need (at minimum):
+
+a private Engine attribute, which we will mark with the keyword final to establish the composition relationship (e.g. private final Engine engine;)
+a private ArrayList to keep track of the Cars currently attached
+a constructor public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity) which will initialize the Engine and Cars and store them
+a couple of accessors:
+public Engine getEngine()
+public Car getCar(int i) to return the ith car
+public int getMaxCapacity() which will return the maximum total capacity across all Cars
+public int seatsRemaining() which will return the number of remaining open seats across all Cars
+and finally, its own public void printManifest() that prints a roster of all Passengers onboard (Hint: your Cars can help!) */
