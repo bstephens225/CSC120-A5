3bfcbc66426f947512ed6565deb64cfe27d5b578
bstephens225 <bstephens@smith.edu>
2023-03-06 20:40:20 -0500

    passenger draft

--- Passenger.java
+++ Passenger.java
@@ -4,5 +4,24 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    public void boardCar(Car c){
+        try {
+            c.addPassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); //no room
+        }
+    }
+
+    public void getOffCar(Car c){
+        try {
+            c.removePassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); //not on board
+        }
+    }
 }
 
+/**Now that you've got a functional Car class, the Passenger class can be expanded to use the Car's methods to implement some of its own:
+public void boardCar(Car c) can call c.addPassenger(this) to board a given Car (Hint: this method should be ready to catch the RuntimeException that will be thrown by c.addPassenger(...) in the event that the car is full.)
+public void getOffCar(Car c) can call c.removePassenger(this) to get off a given Car (Hint: this method should be ready to catch the RuntimeException that will be thrown by c.removePassenger(...) in the event that the Passenger wasn't actually onboard.) */
